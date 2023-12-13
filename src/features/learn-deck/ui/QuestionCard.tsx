cd6e72267fc24f332d5d45aa58bd725851845173
ilyadzh <ilay00@mail.ru>
2023-12-13 09:41:34 +0300

    добавил флип карточки

--- src/features/learn-deck/ui/QuestionCard.tsx
+++ src/features/learn-deck/ui/QuestionCard.tsx
@@ -1,5 +1,5 @@
-import { FC, useEffect, useState } from 'react';
-import { Card, CardHeader, CardFooter, Button, Text } from '@chakra-ui/react';
+import { FC, useEffect, useRef, useState } from 'react';
+import { Card, CardHeader, CardFooter, Button, Text, Box } from '@chakra-ui/react';
 
 import { Card as CardProps } from '@/types/card';
 import { CardFieldMode } from '@/types/card';
@@ -24,35 +24,76 @@ const Content: FC<ContentProps> = ({ mode, value }) => {
 
 export const QuestionCard: FC<CardProps> = ({ id, question, questionMode, answer, answerMode }) => {
     const [isAnswerVisible, setIsAnswerVisible] = useState<boolean>(false);
+    const cardRef = useRef<HTMLDivElement | null>(null);
+    const frontRef = useRef<HTMLDivElement | null>(null);
+    const backRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         setIsAnswerVisible(false);
     }, [id]);
+
+    useEffect(() => {
+        if (cardRef.current && backRef.current && frontRef.current) {
+            if (isAnswerVisible) {
+                cardRef.current.style.height = `${backRef.current.clientHeight}px`;
+            } else {
+                cardRef.current.style.height = `${frontRef.current.clientHeight}px`;
+            }
+        }
+    }, [isAnswerVisible]);
 
     const toggleAnswer = () => {
         setIsAnswerVisible(prev => !prev);
     };
 
     return (
-        <Card variant='outline'>
-            <CardHeader>
-                {isAnswerVisible ? (
-                    <Content mode={answerMode} value={answer} />
-                ) : (
+        <Card
+            ref={cardRef}
+            variant='outline'
+            transition='transform 0.6s, height 0.4s'
+            style={{
+                position: 'relative',
+                transformStyle: 'preserve-3d',
+                transform: isAnswerVisible ? 'rotateY(180deg)' : 'rotateY(0)',
+            }}
+        >
+            <Box
+                ref={frontRef}
+                position='absolute'
+                transition='transform 0.6s'
+                width='100%'
+                style={{
+                    backfaceVisibility: 'hidden',
+                }}
+            >
+                <CardHeader>
                     <Content mode={questionMode} value={question} />
-                )}
-            </CardHeader>
-            <CardFooter p={0}>
-                {isAnswerVisible ? (
-                    <Button width='100%' onClick={toggleAnswer}>
-                        Скрыть ответ
-                    </Button>
-                ) : (
+                </CardHeader>
+                <CardFooter p={0}>
                     <Button width='100%' onClick={toggleAnswer}>
                         Показать ответ
                     </Button>
-                )}
-            </CardFooter>
+                </CardFooter>
+            </Box>
+            <Box
+                ref={backRef}
+                position='absolute'
+                transition='transform 0.6s'
+                width='100%'
+                transform='rotateY(180deg)'
+                style={{
+                    backfaceVisibility: 'hidden',
+                }}
+            >
+                <CardHeader>
+                    <Content mode={answerMode} value={answer} />
+                </CardHeader>
+                <CardFooter p={0}>
+                    <Button width='100%' onClick={toggleAnswer}>
+                        Скрыть ответ
+                    </Button>
+                </CardFooter>
+            </Box>
         </Card>
     );
 };
